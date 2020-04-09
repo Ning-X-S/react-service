aa2b47abeb5043420d58862fab00310338c0045f
higo <higo@higodeMacBook-Pro.local>
2020-04-09 12:45:08 +0800

    ADD:建表sql

---
+++ app/config/creat-sql.js
@@ -0,0 +1,10 @@
+
+// first_test表
+// create table `first_test`(
+//     `id` int(10) not null AUTO_INCREMENT comment '动态id',
+//     `title` varchar(20) not null comment '发送标题',
+//     `desc` varchar(500) not null comment '描述',
+//     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+//     `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+//     PRIMARY KEY (`id`)
+//   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='动态表';
