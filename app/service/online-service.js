4870a55405bf22fe0426e2013597b29fe23eb2c4
higo <higo@higodeMacBook-Pro.local>
2020-04-14 19:37:46 +0800

    FIX:修复python取值错误

--- app/service/online-service.js
+++ app/service/online-service.js
@@ -9,7 +9,7 @@ let isPacking = false
 
 async function onlineServer (ctx, next) {
   let cwd = process.cwd()
-  let { password = '', name = '', type = 'restart', update_modules = "no_update" } = ctx.params
+  let { password = '', name = '', type = 'restart', update_modules = "update_code" } = ctx.params
   if (isPacking) {
     throw new ServiceError({ message: '任务正在进行中', code: 400765 })
   }
@@ -20,7 +20,7 @@ async function onlineServer (ctx, next) {
     throw new ServiceError({ message: '任务查询失败', code: 4000503 })
   }
   if (config.online[name].password !== password) {
-    throw new ServiceError({ message: '密码错误', code: 4000502 })
+    throw new ServiceError({ message: '密码错误' + config.online[name].password, code: 4000502 })
   }
   isPacking = true
   try {
@@ -31,7 +31,7 @@ async function onlineServer (ctx, next) {
   }
   try {
     process.nextTick(async () => {
-      let res = await execSyncNew(`python3 ${config.python}.py ${type} ${update_modules}`)
+      let res = await execSyncNew(`python3 ${config.online[name].python}.py ${type} ${update_modules}`)
       isPacking = false
       console.log(res)
     })
